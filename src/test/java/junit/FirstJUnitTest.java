385940a4e7d6cea45b7943fa187fa7e199f51ca9
Ilya Besedin <ilya@applicatura.com>
2022-02-08 22:09:06 +0300

    added open and close browser

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,9 +1,22 @@
 package junit;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FirstJUnitTest {
+
+    @BeforeEach
+    void openBrowser() {
+        Selenide.open("https://google.com");
+    }
+
+    @AfterEach
+    void closeBrowser () {
+        Selenide.closeWebDriver();
+    }
 
     @Test
     void simpleTest() {
