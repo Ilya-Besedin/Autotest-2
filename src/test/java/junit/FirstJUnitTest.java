751ea9466a3e6c3a2e1b2052b959ed70b376cd08
Ilya Besedin <ilya@applicatura.com>
2022-02-08 21:56:42 +0300

    added first junit test

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,12 @@
+package junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(2 < 3);
+    }
+}
