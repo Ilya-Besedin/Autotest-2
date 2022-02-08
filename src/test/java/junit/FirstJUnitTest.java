da48b08282523a25dbb8695eb4d41de07685f597
Ilya Besedin <ilya@applicatura.com>
2022-02-08 22:17:21 +0300

    first test with before and after structure

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,25 +1,28 @@
 package junit;
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FirstJUnitTest {
 
     @BeforeEach
-    void openBrowser() {
-        Selenide.open("https://google.com");
+    void before() {
+        System.out.println("Этот метод выполняется перед каждым тестом");
     }
 
     @AfterEach
-    void closeBrowser () {
-        Selenide.closeWebDriver();
+    void after () {
+        System.out.println("Этот метод выполняется после каждого теста");
     }
 
     @Test
-    void simpleTest() {
-        Assertions.assertTrue(2 < 3);
+    void textTest() {
+        System.out.println("        Это первый тест");
+    }
+
+    @Test
+    void videoTest() {
+        System.out.println("        Это второй тест");
     }
 }
