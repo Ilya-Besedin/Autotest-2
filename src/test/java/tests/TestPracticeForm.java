83d97cca2a12b820f302e383198aecdff1d26fd2
Ilya Besedin <ilya@applicatura.com>
2022-02-10 00:20:31 +0300

    added table check

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -24,7 +24,7 @@ public class TestPracticeForm {
         //Часть 1. Проверка заполнения обязательных форм
         //заполняем текстовые формы
         $("#firstName").setValue("Ivan");
-        $("#lastName").setValue("123456");
+        $("#lastName").setValue("Leonov");
         $("#userEmail").setValue("test@mail.com");
         $("#userNumber").setValue("0123456789");
         $("#currentAddress").setValue("New York, NY 10004, USA, 17 Broadway");
@@ -58,9 +58,9 @@ public class TestPracticeForm {
 
         //проверяем форму с результатом заполнения данных
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Student Name Max Maximov"), text("Student Email test@mail.com"),
-                text("Gender Male"), text("Mobile 0123456789"), text("Date of Birth 24 February,1999"),
-                text("Subjects Hindi"), text("Hobbies Sports, Reading, Music"), text("Picture JPEG.jpg"),
+        $(".table-responsive").shouldHave(text("Student Name Ivan Leonov"), text("Student Email test@mail.com"),
+                text("Gender Male"), text("Mobile 0123456789"), text("Date of Birth 14 June,1999"),
+                text("Subjects"), text("Hobbies Sports, Music, Reading"), text("Picture JPEG.jpg"),
                 text("Address New York, NY 10004, USA, 17 Broadway"), text("State and City Haryana Karnal"));
 
     }
