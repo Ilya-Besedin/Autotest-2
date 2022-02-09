95891af07a586df138b3050dd8eea1a59e72491d
Ilya Besedin <ilya@applicatura.com>
2022-02-09 23:26:27 +0300

    added some tests

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,5 +20,30 @@ public class TestPracticeForm {
     void successFillTest() {
         open("/automation-practice-form");
         $(".main-header").shouldHave(text("Practice form"));
+
+        //Часть 1. Проверка заполнения обязательных форм
+        //заполняем текстовые формы
+        $("#firstName").setValue("IvAn");
+        $("#lastName").setValue("123456");
+        $("#userEmail").setValue("test@mail.com");
+        $("#userNumber").setValue("0123456789");
+        $("#currentAddress").setValue("New York, NY 10004, USA, 17 Broadway");
+
+        //отмечаем радиобаттоны и чекбоксы
+        $(byText("Male")).click();
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+        $(byText("Reading")).click();
+
+        //открываем дата-пикер
+        $("#dateOfBirthInput").click();
+        //выбираем даты
+        $(".react-datepicker__year-select").selectOptionByValue("1999");
+        $(".react-datepicker__month-select").selectOptionContainingText("June");
+        $(".react-datepicker__day--014").click();
+
+        //кликаем Submit
+        $("#submit").click();
+
     }
 }
