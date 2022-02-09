39868f31d0ed7ce6c2d6f2f25af640fe508409b7
Ilya Besedin <ilya@applicatura.com>
2022-02-10 00:32:05 +0300

    added subject

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -35,6 +35,10 @@ public class TestPracticeForm {
         $(byText("Music")).click();
         $(byText("Reading")).click();
 
+        //выбираем изучаемый предмет
+        $("#subjectsContainer").setValue("ec");
+        $(byText("Economics")).click();
+
         //открываем дата-пикер
         $("#dateOfBirthInput").click();
         //выбираем даты
@@ -52,7 +56,6 @@ public class TestPracticeForm {
         $(byText("Select City")).click();
         $(byText("Karnal")).click();
 
-
         //кликаем Submit
         $("#submit").click();
 
@@ -60,7 +63,7 @@ public class TestPracticeForm {
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text("Student Name Ivan Leonov"), text("Student Email test@mail.com"),
                 text("Gender Male"), text("Mobile 0123456789"), text("Date of Birth 14 June,1999"),
-                text("Subjects"), text("Hobbies Sports, Music, Reading"), text("Picture JPEG.jpg"),
+                text("Subjects"), text("Hobbies Sports, Reading, Music"), text("Picture JPEG.jpg"),
                 text("Address New York, NY 10004, USA, 17 Broadway"), text("State and City Haryana Karnal"));
 
     }
